215b9bb5a14fe0034f8eaddc11286cf0de1bac25
DashaSav <savenkodasa772@gmail.com>
2022-12-10 23:12:02 +0300

    splash_screen again

--- app/src/main/java/com/example/dzen/splash_screen/SplashScreen.java
+++ app/src/main/java/com/example/dzen/splash_screen/SplashScreen.java
@@ -1,13 +1,14 @@
 package com.example.dzen.splash_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.dzen.R;
 import com.example.dzen.onBoarding.OnBoarding;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,7 +16,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_logo);
-
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Intent intent = new Intent(SplashScreen.this, OnBoarding.class);
         startActivity(intent);
     }
